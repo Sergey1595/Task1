bc2168edd0cdbdac68bc766c21a077d84defa725
Sergey1595 <alstar1595@gmail.com>
2019-01-21 16:46:33 +0200

    1.) Change of MainPage.

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,11 +1,46 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.logged.WebDriverWaitLogged;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainPage extends BasePage {
     WebDriver driver = null;
+    WebDriverWaitLogged wait = null;
+    public MainPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+
+        wait = new WebDriverWaitLogged(driver);
+    }
+
+    @FindBy (className = "expand-more _gray-darker hidden-sm-down")
+    private WebElement сurrencyButton;
+
+    @FindBy (xpath = "//span[@class='price']")
+    private List<WebElement> pricesOfProducts;
+
+    @FindBy (xpath = "//a[@title='Доллар США']")
+    private WebElement setDollarCurrenceButton;
 
 
+    public void setDollarCurrence(){
+        сurrencyButton.click();
+        wait.waitForElementToBeClickable(setDollarCurrenceButton);
+        setDollarCurrenceButton.click();
+    }
 
-
+    public List<String> getCurrenceOfProducts(){
+        List<String> currenceOfProducts = new LinkedList<>();
+        for(int i = 0; i < pricesOfProducts.size(); i++){
+            currenceOfProducts.add(pricesOfProducts.get(1).getText());
+        }
+        return currenceOfProducts;
+    }
 }
