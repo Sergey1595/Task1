d05b5348247465caf1dd654ca2e650c724fadad7
Sergey1595 <alstar1595@gmail.com>
2019-02-01 14:48:39 +0200

    777

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -29,7 +29,7 @@ public abstract class BaseTest {
 
         driver.navigate().to(Properties.getBaseUrl());
     }
-
+    //TestBranch
     @AfterClass
     public void tearDown() {
         if (driver != null) {
